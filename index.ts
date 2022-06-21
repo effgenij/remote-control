23a2cc782c15788c473c55355d29a0464a0f0d4c
effgenij <effgenij@gmail.com>
2022-06-21 22:34:48 +0300

    Add basic websocket server

--- index.ts
+++ index.ts
@@ -7,3 +7,18 @@ const HTTP_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', ws => {
+    ws.on('message', data => {
+        console.log('received: %s', data);
+    });
+
+    ws.send('something');
+    ws.send('test');
+});
+
+wss.on('close', () => {
+    console.log('End');
+});
