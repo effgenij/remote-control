4b06f693102ea7ade9c76b126fa54987b010efea
effgenij <effgenij@gmail.com>
2022-06-21 17:56:00 +0300

    add eslint

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -2,10 +2,10 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
 
-export const httpServer = http.createServer(function (req, res) {
-  const __dirname = path.resolve(path.dirname(''));
-  const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-  fs.readFile(file_path, function (err, data) {
+const httpServer = http.createServer((req, res) => {
+  const dirname = path.resolve(path.dirname(''));
+  const filePath = dirname + (req.url === '/' ? '/front/index.html' : `/front${req.url}`);
+  fs.readFile(filePath, (err, data) => {
     if (err) {
       res.writeHead(404);
       res.end(JSON.stringify(err));
@@ -15,3 +15,5 @@ export const httpServer = http.createServer(function (req, res) {
     res.end(data);
   });
 });
+
+export default httpServer;
