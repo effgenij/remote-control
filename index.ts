5153b9e0f8ac45d8c4770e3d4ba5332962e00a45
effgenij <effgenij@gmail.com>
2022-06-26 23:12:52 +0300

    typo end eol

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@
 import { createWebSocketStream, WebSocketServer } from 'ws';
 import httpServer from './src/http_server/index';
 import controller from './src/controller'
+import os from "os";
 
 const HTTP_PORT = 3000;
 const WEBSOCKET_PORT = 8080;
@@ -12,20 +13,20 @@ httpServer.listen(HTTP_PORT);
 const wss = new WebSocketServer({ port: WEBSOCKET_PORT });
 
 wss.on('connection', ws => {
-    console.log(`Start websocket server on the ${WEBSOCKET_PORT} port!`);
+    console.log(`Start websocket server on the ${WEBSOCKET_PORT} port!${os.EOL}`);
     const wsStream = createWebSocketStream(ws, { encoding: 'utf8', decodeStrings: false });
     wsStream.on('data', async (chunk) => {
         const [command, x, y] = chunk.split(' ');
-        process.stdout.write(`Recieved: ${chunk}`);
+        process.stdout.write(`Received: ${chunk}${os.EOL}`);
         wsStream.write(command);
         try {
             const response = await controller(command, x, y);
             if (typeof(response) === 'string') {
             wsStream.write(response);
             }
-            process.stdout.write(`Command: "${command}" executed without errors.`);
+            process.stdout.write(`Command: "${command}" executed without errors.${os.EOL}`);
         } catch (err: any) {
-            process.stdout.write(`Command: "${command}" not executed. Error message: ${err.message}`);
+            process.stdout.write(`Command: "${command}" not executed.${os.EOL} Error message: ${err.message}${os.EOL}`);
         };
     });
     wsStream.on('end', () => {
